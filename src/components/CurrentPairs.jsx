392e34178cbb98cf483debd2473638eef6a9fd0e
mbelesiu <matt.belesiu@gmail.com>
2021-01-22 11:58:03 -0800

    in progresso

--- src/components/CurrentPairs.jsx
+++ src/components/CurrentPairs.jsx
@@ -8,14 +8,17 @@ const CurrentPairs = (props) => {
   return (
     <PairsContainer>
       { !currentPairs
-      ? null
-      : currentPairs.map((pairArr, index) => (
-        <Pair
-          key={`pair-${index}`}
-          roster={roster} 
-          pairArr={pairArr} 
-        />)
-      )}
+        ? null
+        : currentPairs.map((pairArr, index) => (
+          <Pair
+            key={`pair-${index}`}
+            roster={roster}
+            pairArr={pairArr}
+          />)
+        )}
+      {!currentPairs
+        ? <ExportButtonDis disabled>Export</ExportButtonDis>
+        : <ExportButton>Export</ExportButton>}
     </PairsContainer>
   )
 };
@@ -32,6 +35,25 @@ const PairsContainer = styled.div`
   flex-direction: column;
   width: 75%;
   height: 70%;
+`
+const ExportButton = styled.button`
+  background: linear-gradient(to bottom right, rgb(60, 122, 255), rgb(28, 85, 208));
+  border: 1px solid rgb(55, 66, 92);
+  padding: 10px;
+  color: white;
+  border-radius: 10px;
+  box-shadow: 1px 2px 5px rgb(120, 128, 146);
+  margin: 10px;
+`
+const ExportButtonDis = styled.button`
+  background: linear-gradient(to bottom right, rgb(60, 122, 255), rgb(28, 85, 208));
+  border: 1px solid rgb(55, 66, 92);
+  padding: 10px;
+  color: white;
+  border-radius: 10px;
+  box-shadow: 1px 2px 5px rgb(120, 128, 146);
+  margin: 10px;
+  opacity: 0.3;
 `
 
 export default CurrentPairs;
